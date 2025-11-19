2b6369a9b9f99fcbb89fa6218cde8976dc5192ef
mbewustanley <mbewustanley@gmail.com>
2025-11-19 17:00:19 +0100

    modifying requirements and ipynb, and data preprocessing pipeline testing and output

---
+++ Pipeline/Data_preprocessing.py
@@ -0,0 +1,143 @@
+
+import os
+import warnings
+#from pathlib import Path
+
+import pandas as pd
+
+# Mute warnings
+warnings.filterwarnings('ignore')
+
+#To clean the data
+def clean(df):
+    df["Exterior2nd"] = df["Exterior2nd"].replace({"Brk Cmn": "BrkComm"})
+    # Some values of GarageYrBlt are corrupt, so we'll replace them
+    # with the year the house was built
+    df["GarageYrBlt"] = df["GarageYrBlt"].where(df.GarageYrBlt <= 2010, df.YearBuilt)
+    # Names beginning with numbers are awkward to work with
+    df.rename(columns={
+        "1stFlrSF": "FirstFlrSF",
+        "2ndFlrSF": "SecondFlrSF",
+        "3SsnPorch": "Threeseasonporch",
+    }, inplace=True,
+    )
+    return df
+
+
+#To encode the data
+
+# The nominative (unordered) categorical features
+features_nom = ["MSSubClass", "MSZoning", "Street", "Alley", "LandContour", "LotConfig", "Neighborhood", "Condition1", "Condition2",
+                "BldgType", "HouseStyle", "RoofStyle", "RoofMatl", "Exterior1st", "Exterior2nd", "MasVnrType", "Foundation", "Heating",
+                "CentralAir", "GarageType", "MiscFeature", "SaleType", "SaleCondition"]
+
+
+# The ordinal (ordered) categorical features
+
+# Pandas calls the categories "levels"
+five_levels = ["Po", "Fa", "TA", "Gd", "Ex"]
+ten_levels = list(range(10))
+
+ordered_levels = {
+    "OverallQual": ten_levels,
+    "OverallCond": ten_levels,
+    "ExterQual": five_levels,
+    "ExterCond": five_levels,
+    "BsmtQual": five_levels,
+    "BsmtCond": five_levels,
+    "HeatingQC": five_levels,
+    "KitchenQual": five_levels,
+    "FireplaceQu": five_levels,
+    "GarageQual": five_levels,
+    "GarageCond": five_levels,
+    "PoolQC": five_levels,
+    "LotShape": ["Reg", "IR1", "IR2", "IR3"],
+    "LandSlope": ["Sev", "Mod", "Gtl"],
+    "BsmtExposure": ["No", "Mn", "Av", "Gd"],
+    "BsmtFinType1": ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"],
+    "BsmtFinType2": ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"],
+    "Functional": ["Sal", "Sev", "Maj1", "Maj2", "Mod", "Min2", "Min1", "Typ"],
+    "GarageFinish": ["Unf", "RFn", "Fin"],
+    "PavedDrive": ["N", "P", "Y"],
+    "Utilities": ["NoSeWa", "NoSewr", "AllPub"],
+    "CentralAir": ["N", "Y"],
+    "Electrical": ["Mix", "FuseP", "FuseF", "FuseA", "SBrkr"],
+    "Fence": ["MnWw", "GdWo", "MnPrv", "GdPrv"],
+}
+
+# Add a None level for missing values
+ordered_levels = {key: ["None"] + value for key, value in
+                  ordered_levels.items()}
+
+
+def encode(df):
+    # Nominal categories
+    for name in features_nom:
+        df[name] = df[name].astype("category")
+        # Add a None category for missing values
+        if "None" not in df[name].cat.categories:
+            df[name] = df[name].cat.add_categories("None")
+    # Ordinal categories
+    for name, levels in ordered_levels.items():
+        df[name] = df[name].astype(CategoricalDtype(levels,
+                                                    ordered=True))
+    return df
+
+
+#Handling Missing data
+def impute(df):
+    for name in df.select_dtypes("number"):
+        df[name] = df[name].fillna(0)
+    for name in df.select_dtypes("category"):
+        df[name] = df[name].fillna("None")
+    return df
+
+
+#To load the data
+def load_data():
+    # Read data
+    df_train = pd.read_csv("Input_data/train.csv", index_col="Id")
+    df_test = pd.read_csv("Input_data/test.csv", index_col="Id")
+
+    # Merge the splits so we can process them together
+    df = pd.concat([df_train, df_test])
+
+    # Preprocessing
+    df = clean(df)
+    df = encode(df)
+    df = impute(df)
+
+    # Reform splits
+    df_train = df.loc[df_train.index, :]
+    df_test = df.loc[df_test.index, :]
+    return df_train, df_test
+
+df_train, df_test = load_data()
+
+
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
+    try:
+        data_path = os.path.join(data_path, 'raw')
+        os.makedirs(data_path, exist_ok=True)
+        df_train.to_csv(os.path.join(data_path, "df_train.csv"), index=False)
+        df_test.to_csv(os.path.join(data_path, "df_test.csv"), index=False)
+    except Exception as e:
+        print(f"Error: An unexpected error occurred while saving the data.")
+        print(e)
+        raise
+
+
+def main():
+    try:
+        df_train, df_test = load_data()
+        save_data(df_train, df_test, data_path='data')
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Failed to complete the data preprocessing.")
+
+if __name__ == '__main__':
+    main()
+
+
+
